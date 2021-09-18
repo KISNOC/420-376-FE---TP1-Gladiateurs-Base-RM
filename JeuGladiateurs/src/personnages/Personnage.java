4fcb2bac26aa3454e92248d889f54de9345bade4
ETI <ETI@PO-21477>
2021-09-18 12:39:15 -0400

    Initialisation des personnages

--- JeuGladiateurs/src/personnages/Personnage.java
+++ JeuGladiateurs/src/personnages/Personnage.java
@@ -2,37 +2,79 @@ package personnages;
 
 public class Personnage {
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Attributs">
     // TODO : Mettre vos attributs ici
+    String nom;
+    int pointsDeVie;
+    int valeurMaxAttaque;
+    int valeurDefense;
+    int initiative;
     // </editor-fold>
-
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
+    
     // <editor-fold defaultstate="collapsed" desc="Constructeurs et initialisation">
     public Personnage(String nom, int attaqueMax, int defense, int pvs, int ini) {
         // TODO : Constructeur AVEC paramètres
+        this.nom = nom;
+        this.pointsDeVie = pvs;
+        this.valeurMaxAttaque = attaqueMax;
+        this.valeurDefense = defense;
+        this.initiative = ini;
     }
 
     public Personnage() {
         // TODO : Constructeur SANS paramètres qui initialise à ZÉRO ou à ""
+        String nom = "";
+        int pointsDeVie = -1;
+        int valeurMaxAttaque = -1;
+        int valeurDefense = -1;
+        int initiative = -1;
     }
     // </editor-fold>
+    public String getNom() {
+        return nom;
+        // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
+    }
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
+    public int getPointsDeVie() {
+        return pointsDeVie;
+    }
+
+    public int getValeurMaxAttaque() {
+        return valeurMaxAttaque;
+    }
+
+    public int getValeurDefense() {
+        return valeurDefense;
+    }
+
     // <editor-fold defaultstate="collapsed" desc="Getters et setters">
     // TODO : Les getters
-    // TODO : Les setters
-    // </editor-fold>
+    public int getInitiative() {    
+        return initiative;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+        // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
+    }
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
+    public void setPointsDeVie(int pointsDeVie) {
+        this.pointsDeVie = pointsDeVie;
+    }
+
+    public void setValeurMaxAttaque(int valeurMaxAttaque) {
+        this.valeurMaxAttaque = valeurMaxAttaque;
+    }
+
+    public void setValeurDefense(int valeurDefense) {
+        this.valeurDefense = valeurDefense;
+    }
+
+    // TODO : Les setters
+    public void setInitiative(int initiative) {    
+        this.initiative = initiative;
+    }
+
+    // </editor-fold>
     // <editor-fold defaultstate="collapsed" desc="Mécanique de jeu">
     public void afficherInfosPersonnage() {
         // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
